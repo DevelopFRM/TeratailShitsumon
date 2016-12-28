0cc986e1c2d99acddd25565dd38c6fe1330ab16b
RyuusukeMatsumoto <matsumotokaka1102@gmail.com>
2016-12-28 20:44:16 +0900

    プロジェクト上げ
    BulletGeneratorのシーンファイルを追加

---
+++ Assets/Resources/BulletGenerator/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += this.transform.forward * speed;
+
+		if ( 50 < this.transform.position.z )
+			this.gameObject.SetActive( false );
+	}
+
+
+}
---
+++ Assets/Resources/BulletGenerator/BulletGenerator.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+//------------------------------------------------------------------------------
+//==============================================================================
+// 弾プール.
+public class BulletGenerator : MonoBehaviour {
+
+
+	[SerializeField] private GameObject[] bullets = null;	// 子要素に入っている弾プール.
+	
+
+	//------------------------------------------------------------------------------
+	void Start() {
+
+		// 子要素の弾たちを配列にしている.
+		Queue< GameObject > q = new Queue< GameObject >();
+		for ( int i = 0; i < this.transform.childCount; ++i ) {
+			q.Enqueue( this.transform.GetChild( i ).gameObject );
+		}
+		this.bullets = q.ToArray();
+		q.Clear();
+		
+	}
+
+	
+	//------------------------------------------------------------------------------
+	void Update() {}
+
+
+	//------------------------------------------------------------------------------
+	public void shot(
+			Transform origin ) {
+
+		// 子要素の配列の中から使えるものを探し、あれば使いまわす.
+		for ( int i = 0; i < this.bullets.Length; ++i ) {
+			if ( !this.bullets[ i ].activeSelf ) {
+				this.bullets[ i ].SetActive( true );
+				this.bullets[ i ].transform.position = origin.position;
+				this.bullets[ i ].transform.rotation = origin.rotation;
+				break;
+			}
+		}
+	}
+	
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
